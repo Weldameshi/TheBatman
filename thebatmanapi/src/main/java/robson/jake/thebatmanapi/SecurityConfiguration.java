9d85e47d46e42a123fff0c49e126e8b76dc4e4b7
Weldameshi <47304615+Weldameshi@users.noreply.github.com>
2020-12-07 15:31:02 -0700

    jpa

--- thebatmanapi/src/main/java/robson/jake/thebatmanapi/SecurityConfiguration.java
+++ thebatmanapi/src/main/java/robson/jake/thebatmanapi/SecurityConfiguration.java
@@ -10,9 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -20,13 +18,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
-
 import robson.jake.thebatmanapi.model.User;
 import robson.jake.thebatmanapi.repository.UserRepository;
-import robson.jake.thebatmanapi.security.AuthSuccessHandler;
-import robson.jake.thebatmanapi.security.LogoutSuccess;
 
 
 @Configuration
